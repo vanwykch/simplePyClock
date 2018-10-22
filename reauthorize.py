de3af6cae6fbb0caa878215b9c5899e843edd3ef
Chris <you@example.com>
2018-10-21 20:01:18 -0600

    Took credentials out of reauthorize.py

--- reauthorize.py
+++ reauthorize.py
@@ -12,6 +12,19 @@ redirect_uri='http://127.0.0.1:8080/'
 ACCESS_TOKEN = 'no access'
 REFRESH_TOKEN = 'no refesh'
 
+
+
+def get_client_and_secret():
+    with open('cred.json') as file:
+        data = json.load(file)
+
+    print('client: ' + data['client'])
+    print('secret: ' + data['secret'])
+    CLIENT_ID = data['client']
+    CLIENT_SECRET = data['secret']
+
+
+get_client_and_secret    
 server = Oauth2.OAuth2Server(CLIENT_ID, CLIENT_SECRET)
 server.browser_authorize()
 ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
